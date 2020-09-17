c827a04e9a67d126ef6595bec6e884255515b50b
Liselle Logan <liselle.logan@gmail.com>
2020-09-17 03:17:01 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,17 @@
-# Write your code here.
+require 'pry'
+
+def line(people)
+  if people.size > 0
+    customers = " "
+    people.map.with_index(1) do |person, index|
+      puts customers << "#{index}. #{person} "
+    end
+    puts "The line is currently: #{customers}"
+  else
+    puts "The line is currently empty."
+  end
+end
+
+def take_a_number(people, name)
+  line(people)
+end
