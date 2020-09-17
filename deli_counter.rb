3a78a222b32e26401116dace202b606b916dfb98
Liselle Logan <liselle.logan@gmail.com>
2020-09-17 22:54:24 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,20 +1,5 @@
 require 'pry'
 
-#def line(people)
-#  if people.size > 0
-#    position = people.map.with_index(1) {|person, index| "#{index}. #{person}"}
-#    stringified_position = position.join(" ")
-#    puts "The line is currently: " + stringified_position
-#    end
-#  else
-#    puts "The line is currently empty."
-#  end
-#end
-
-#def take_a_number(people, name)
-#  line(people)
-#end
-
 def line(people)
   if people.length == 0
     puts "The line is currently empty."
